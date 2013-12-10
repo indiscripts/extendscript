eef86cdf5b8b21ba564bcd0ebfa26b8e2b5bf204
indiscripts <marc@indiscripts.com>
2013-12-10 16:57:32 +0100

    Create test.jsx

---
+++ muZ/test.jsx
@@ -0,0 +1,24 @@
+# include 'muZ.jsx'
+
+
+_('EXPRESSION')('LITERAL')
+({
+    EXPRESSION: LITERAL
+              | _/"NAME"
+              | _/'(' + EXPRESSION + _/')'
+              | _/"PREFIX_OP" + EXPRESSION
+              | EXPRESSION +
+                  (
+                    _/"INFIX_OP" + EXPRESSION
+                  | _/'?' + EXPRESSION + _/':' + EXPRESSION
+                  | _/"INVOCATION"
+                  | _/"REFINEMENT"
+                  )
+              | _/'new' + EXPRESSION + _/"INVOCATION"
+              | _/'delete' + EXPRESSION + _/"REFINEMENT"
+    ,
+    LITERAL: _/"a-z"
+    ,
+});
+
+alert( _.EXPRESSION );
